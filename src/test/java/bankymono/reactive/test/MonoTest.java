d6ed32a2d6b4fecc51475a0c0317e73eae5b39a4
Ayodeji Bankole <bankymono@gmail.com>
2025-06-30 20:05:39 +0100

    first commit

---
+++ src/test/java/bankymono/reactive/test/MonoTest.java
@@ -0,0 +1,13 @@
+package bankymono.reactive.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+@Slf4j
+public class MonoTest {
+
+    @Test
+    public void test() {
+        log.info("Working as intended");
+    }
+}
