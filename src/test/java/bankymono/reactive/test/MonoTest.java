feae6fd06939e460fb70598ebdb39de471fc97c0
Ayodeji Bankole <Ayodeji.Bankole@Interswitchng.com>
2025-07-04 21:27:16 +0100

    #

--- src/test/java/bankymono/reactive/test/MonoTest.java
+++ src/test/java/bankymono/reactive/test/MonoTest.java
@@ -2,12 +2,56 @@ package bankymono.reactive.test;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 @Slf4j
 public class MonoTest {
 
     @Test
-    public void test() {
+    public void monoSubscriber() {
+        String name = "Bankole Ayodeji";
+        Mono<String> mono = Mono.just(name)
+                .log();
+        mono.subscribe();
+        log.info("------------------------------");
+        StepVerifier.create(mono)
+                        .expectNext(name)
+                                .verifyComplete();
+//        log.info("Mono -> {}", mono);
+        log.info("Working as intended");
+    }
+
+    @Test
+    public void monoSubscriberConsumer() {
+        String name = "Bankole Ayodeji";
+        Mono<String> mono = Mono.just(name)
+                .log();
+        mono.subscribe((val) -> log.info("value -> {}", val));
+        log.info("------------------------------");
+//        StepVerifier.create(mono)
+//                .expectNext(name)
+//                .verifyComplete();
+//        log.info("Mono -> {}", mono);
+        log.info("Working as intended");
+    }
+
+    @Test
+    public void monoSubscriberConsumerError() {
+        String name = "Bankole Ayodeji";
+
+        Mono<String> mono = Mono.just(name)
+                .map(s -> {
+                    throw new RuntimeException("Testing mono with error");
+                });
+
+        mono.subscribe((val) -> log.info("value -> {}", val), s -> log.error("Something Bad happend"));
+        log.info("------------------------------");
+        StepVerifier.create(mono)
+//                .expectNext(name)
+                .expectError(RuntimeException.class)
+                .verify();
+//        log.info("Mono -> {}", mono);
         log.info("Working as intended");
     }
 }
